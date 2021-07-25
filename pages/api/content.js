e2892868bf362fc8ab20fe681332ad2932e16954
andheller <aheller9@gmail.com>
2021-07-25 11:49:34 -0500

    2 files modified

--- pages/api/content.js
+++ pages/api/content.js
@@ -1,11 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['GET', 'HEAD']
+});
 
 export default function content(req, res) {
-  res
-    .status(200)
-    .json({
-      component: 'header',
-      tagline: 'Hello World!',
-      array: [{ component: 'header',tagline:'test 1' }, { component: 'hero', tagline:'test 2' }]
-    });
+  res.status(200).json({
+    component: 'header',
+    tagline: 'Hello World!',
+    array: [
+      { component: 'header', tagline: 'test 1' },
+      { component: 'hero', tagline: 'test 2' }
+    ]
+  });
 }
