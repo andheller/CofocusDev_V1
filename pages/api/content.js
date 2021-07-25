158c950cd8967efd3b1b85088be58ce6582eef7d
andheller <aheller9@gmail.com>
2021-07-25 12:43:45 -0500

    3 files modified

--- pages/api/content.js
+++ pages/api/content.js
@@ -1,8 +1,14 @@
 import Cors from 'cors';
+import initMiddleware from '../../lib/init-middleware';
 
-const cors = Cors({
-  methods: ['GET', 'HEAD']
-});
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS']
+  })
+);
 
 export default function content(req, res) {
   res.status(200).json({
